9bb56d7bf8e9c5984d69c6d0c9fbd3b3ee5c9e70
XZM1415 <18556951750@163.com>
2022-05-13 20:06:01 +0800

    测试控制类

--- src/xpackage/Test_control.java
+++ src/xpackage/Test_control.java
@@ -1,13 +1,48 @@
 package xpackage;
 
-import xpackage.weapons.ExampleWeapon;
-import xpackage.weapons.Weapon;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+import xpackage.mobs.ExampleMob;
+import xpackage.mobs.Mob;
+import xpackage.weapons.*;
 
 //测试控制类
 public class Test_control {
+    /**
+     * 目前看来
+     * 方法一切正常
+     * 希望如此
+     * @param args
+     */
     public static void main(String[] args) {
-        ExampleWeapon wp = new ExampleWeapon();
-        // System.out.println(wp.Attack(100.0,20.0));
-        System.out.println(wp.Attack(100.0, 10000.0));
+        JFrame jf = new TFrame();
+        Weapon weapon = new ExampleWeapon();
+        Mob mob = new ExampleMob();
+        JButton jb = new JButton("Text");
+        jb.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mob.SETHP(weapon.Attack(mob.GETHP(), mob.GETDEF()));
+                System.out.println(mob.GETHP());
+            }
+            
+        });
+        jf.add(jb);
     }    
+}
+class TFrame extends JFrame{
+    public TFrame(){
+        setTitle("测试窗口");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(100,100,200,200);
+        setLayout(new FlowLayout());
+
+        setVisible(true);
+    }
 }
