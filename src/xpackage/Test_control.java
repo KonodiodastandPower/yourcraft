2352c93c6917b8728d29f105124b378853332e68
XZM1415 <18556951750@163.com>
2022-05-14 21:56:35 +0800

    控制测试

--- src/xpackage/Test_control.java
+++ src/xpackage/Test_control.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
+import xpackage.bosses.Boss;
+import xpackage.bosses.ExampleBoss;
 import xpackage.mobs.ExampleMob;
 import xpackage.mobs.Mob;
 import xpackage.weapons.*;
@@ -17,30 +19,32 @@ public class Test_control {
      * 目前看来
      * 方法一切正常
      * 希望如此
-     * @param args
      */
     public static void main(String[] args) {
         JFrame jf = new TFrame();
         Weapon weapon = new ExampleWeapon();
-        Mob mob = new ExampleMob();
-        JButton jb = new JButton("Text");
+//        Mob mob = new ExampleMob();
+        Boss boss = new ExampleBoss();
+        JButton jb = new JButton("攻击");
+        System.out.println(weapon.GETdamage());
         jb.addActionListener(new ActionListener(){
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                mob.SETHP(weapon.Attack(mob.GETHP(), mob.GETDEF()));
-                System.out.println(mob.GETHP());
+                boss.SETHP(weapon.Attack(boss.GETHP(), boss.GETDEF()));
+                System.out.println(boss.GETHP());
             }
             
         });
         jf.add(jb);
+        jf.validate();
     }    
 }
 class TFrame extends JFrame{
     public TFrame(){
         setTitle("测试窗口");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setBounds(100,100,200,200);
+        setBounds(100,100,300,300);
         setLayout(new FlowLayout());
 
         setVisible(true);
