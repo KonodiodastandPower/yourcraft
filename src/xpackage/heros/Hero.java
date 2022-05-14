c85e31e751856acf8fb31249b5bd00ac0ab35ba8
XZM1415 <18556951750@163.com>
2022-05-14 19:22:55 +0800

    英雄类

--- src/xpackage/heros/Hero.java
+++ src/xpackage/heros/Hero.java
@@ -5,6 +5,9 @@ public class Hero {
      * 英雄（人物）的基类
      * 施工中...
      */
-	private Double HP;//生命值
-	private Double DEF;//防御力
+	private Double HP;//英雄的生命值
+	private Double DEF;//英雄的基本防御力
+	private Double hungry;//饥饿值
+	private Double level;//等级
+	
 }
