9ee8b98a6d1cf3da90ec8247cc7e691e22d2cc2c
XZM1415 <18556951750@163.com>
2022-05-15 15:40:01 +0800

    英雄例子

---
+++ src/xpackage/heros/ExampleHero.java
@@ -0,0 +1,46 @@
+package xpackage.heros;
+
+public class ExampleHero extends Hero{
+	private String name = "HERO";//名字
+	private Double HP = 300.0;//英雄的生命值
+	private Double DEF = 20.0;//英雄的基本防御力
+	private Integer hungry = 100;//饥饿值 最高为100
+	private Integer level = 0;//等级
+	private Double experience = 0.0;//经验值
+	public String GETname() {
+		return this.name;
+	}
+	public Double GETHP() {
+		return this.HP;
+	}
+	public Double GETDEF() {
+		return this.DEF;
+	}
+	public Integer GEThungry() {
+		return this.hungry;
+	}
+	public Integer GETlevel() {
+		return this.level;
+	}
+	public Double GETexperience() {
+		return this.experience;
+	}
+	public void SETname(String name) {
+		this.name = name;
+	}
+	public void SETHP(Double HP) {
+		this.HP = HP;
+	}
+	public void SETDEF(Double DEF) {
+		this.DEF = DEF;
+	}
+	public void SEThungry(Integer hungry) {
+		this.hungry = hungry;
+	}
+	public void SETlevel(Integer level) {
+		this.level = level;
+	}
+	public void SETexperience(Double experience) {
+		this.experience = experience;
+	}
+}
