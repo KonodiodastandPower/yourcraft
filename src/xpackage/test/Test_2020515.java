d0ff25c0f3718f32dd7699d1583032e64a8bdfea
XZM1415 <18556951750@163.com>
2022-05-15 15:43:47 +0800

    武器的例子

--- src/xpackage/test/Test_2020515.java
+++ src/xpackage/test/Test_2020515.java
@@ -11,6 +11,8 @@ import javax.swing.JTextArea;
 import xpackage.XFrame;
 import xpackage.bosses.Boss;
 import xpackage.bosses.ExampleBoss;
+import xpackage.heros.ExampleHero;
+import xpackage.heros.Hero;
 import xpackage.mobs.ExampleMob;
 import xpackage.mobs.Mob;
 import xpackage.weapons.ExampleWeapon;
@@ -19,7 +21,6 @@ import xpackage.weapons.Weapon;
 public class Test_2020515 {
 	/**
 	 *这是测试
-	 *英雄类暂时没写完，所以就先不测试英雄挨打了 
 	 */
 	public static void main(String[] args) {
 		XFrame xf = new XFrame();
@@ -27,13 +28,14 @@ public class Test_2020515 {
 		xf.setLayout(new BorderLayout());
 		
 		ExampleWeapon weapon = new ExampleWeapon();
-		Mob mob = new ExampleMob();
-		Boss boss = new ExampleBoss();
+		ExampleMob mob = new ExampleMob();
+		ExampleBoss boss = new ExampleBoss();
+		Hero hero = new ExampleHero();
 		
 		JTextArea jta = new JTextArea();
 		JScrollPane jsp = new JScrollPane(jta);
 		
-		PrintAll(weapon,mob,boss,jta);
+		PrintAll(weapon,mob,boss,hero,jta);
 		
 		JButton jb = new JButton("攻击boss");
 		JButton jb2 = new JButton("攻击mob");
@@ -43,7 +45,8 @@ public class Test_2020515 {
 			public void actionPerformed(ActionEvent e) {
 				jta.append("\n你攻击了老板");
 				boss.SETHP(weapon.Attack(boss.GETHP(), boss.GETDEF(),jta));
-				
+				jta.append("\n你遭到了老板的反击");
+				hero.SETHP(boss.Attack(hero.GETHP(), hero.GETDEF(),jta));
 			}
 		});
 		jb2.addActionListener(new ActionListener() {
@@ -53,6 +56,8 @@ public class Test_2020515 {
 				// TODO 自动生成的方法存根
 				jta.append("\n你攻击了小怪");
 				mob.SETHP(weapon.Attack(mob.GETHP(), mob.GETDEF(),jta));
+				jta.append("\n你遭到了小怪的反击");
+				hero.SETHP(mob.Attack(hero.GETHP(), hero.GETDEF(),jta));
 			}
 		});
 		
@@ -64,8 +69,9 @@ public class Test_2020515 {
 		xf.validate();
 	}
 
-	private static void PrintAll(Weapon weapon,Mob mob,Boss boss,JTextArea jtextarea) {
-		jtextarea.append("武器名称:"+weapon.GETname()+";武器伤害:"+weapon.GETdamage()+";武器暴击率:"+weapon.GETcritical_sr()+"%");
+	private static void PrintAll(Weapon weapon,Mob mob,Boss boss,Hero hero,JTextArea jtextarea) {
+		jtextarea.append("你的名字:"+hero.GETname()+";你的生命值:"+hero.GETHP()+";你的基本防御力:"+hero.GETDEF());
+		jtextarea.append("\n武器名称:"+weapon.GETname()+";武器伤害:"+weapon.GETdamage()+";武器暴击率:"+weapon.GETcritical_sr()+"%");
 		jtextarea.append("\n武器是否无视防御:"+weapon.GETarmor_breaking()+";武器耐久度:"+weapon.GETendurance());
 		jtextarea.append("\n小怪名称:"+mob.GETname()+";小怪伤害:"+mob.GETdamage()+";小怪生命:"+mob.GETHP()+";小怪防御力:"+mob.GETDEF());
 		jtextarea.append("\n老板名称:"+boss.GETname()+";老板伤害:"+boss.GETdamage()+";老板暴击率:"+boss.GETcritical_sr()+"%"+";老板生命值:"+boss.GETHP()+";老板防御力:"+boss.GETDEF());
