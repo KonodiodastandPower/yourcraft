8c5fa0a0fbef16537099b72b8cfefbe035a2170b
XZM1415 <18556951750@163.com>
2022-05-20 23:00:31 +0800

    盔甲类

--- src/xpackage/armors/Armor.java
+++ src/xpackage/armors/Armor.java
@@ -1,8 +1,22 @@
 package xpackage.armors;
 
-public class Armor {
+public class Armor implements Armors{
     /**
      * 所有护甲的基类
-     * 施工中...
+     * 已取得阶段性成功
      */
+	private Double DEF;//盔甲的防御力
+	private Integer endurance;//盔甲的耐久度
+	public Double GETDEF() {
+		return this.DEF;
+	}
+	public Integer GETendurance() {
+		return this.endurance;
+	}
+	public void SETDEF(Double DEF) {
+		this.DEF = DEF;
+	}
+	public void SETendurance(Integer endurance) {
+		this.endurance = endurance;
+	}
 }
