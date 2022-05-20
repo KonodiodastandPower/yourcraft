e38f425b93eea5c8347e97f544161e4e19b24b11
XZM1415 <18556951750@163.com>
2022-05-20 23:03:12 +0800

    英雄例子

--- src/xpackage/heros/ExampleHero.java
+++ src/xpackage/heros/ExampleHero.java
@@ -1,5 +1,8 @@
 package xpackage.heros;
 
+import xpackage.armors.Armor;
+import xpackage.armors.ExampleArmor;
+
 public class ExampleHero extends Hero{
 	private String name = "HERO";//名字
 	private Double HP = 300.0;//英雄的生命值
@@ -7,6 +10,7 @@ public class ExampleHero extends Hero{
 	private Integer hungry = 100;//饥饿值 最高为100
 	private Integer level = 0;//等级
 	private Double experience = 0.0;//经验值
+	private Armor armor = new ExampleArmor();//英雄的护甲
 	public String GETname() {
 		return this.name;
 	}
@@ -25,6 +29,9 @@ public class ExampleHero extends Hero{
 	public Double GETexperience() {
 		return this.experience;
 	}
+	public Armor GETarmor() {
+		return this.armor;
+	}
 	public void SETname(String name) {
 		this.name = name;
 	}
@@ -42,5 +49,8 @@ public class ExampleHero extends Hero{
 	}
 	public void SETexperience(Double experience) {
 		this.experience = experience;
+	}
+	public void SETarmor(Armor armor) {
+		this.armor = armor;
 	}
 }
