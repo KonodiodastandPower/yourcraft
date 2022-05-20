c7b0e4538448c6b04527109691663067574ea2ed
XZM1415 <18556951750@163.com>
2022-05-20 23:00:12 +0800

    杂牌窗口

--- src/xpackage/XFrame.java
+++ src/xpackage/XFrame.java
@@ -6,7 +6,7 @@ public class XFrame extends JFrame{
     public XFrame(){//构造方法
         setTitle("Yourscraft");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(200, 200, 400, 400);
+        setBounds(200, 200, 600, 400);
         setLayout(null);
         setVisible(true);
     }
