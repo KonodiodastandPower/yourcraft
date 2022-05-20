dc21c7fc927ed77034b15af562513f007b7db283
XZM1415 <18556951750@163.com>
2022-05-20 22:58:35 +0800

    盔甲类

---
+++ src/xpackage/armors/Armors.java
@@ -0,0 +1,11 @@
+package xpackage.armors;
+
+public interface Armors {
+	/**
+	 * 无意义的接口
+	 */
+	public Double GETDEF();
+	public Integer GETendurance();
+	public void SETDEF(Double DEF);
+	public void SETendurance(Integer endurance);
+}
